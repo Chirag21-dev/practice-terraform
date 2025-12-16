efa87d59307c944d3d4c638ff6bd71a6eaab89f0
Chirag21-dev <chiragks21@outlook.com>
2025-12-16 17:46:18 +0530

    Created Main.tf for ec2 and s3 bucket.

---
+++ Main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "my_server" {
+  ami           = "ami-03695d52f0d883f65"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "aviz-tf-test"
+  }
+}
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "aviz-hcp-test-121211"
+  tags = {
+    Environment = "PublicAccessDemo"
+  }
+}
