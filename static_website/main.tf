5ff61dd12a7a49df42147a869c344aea9ac18641
Chirag21-dev <chiragks21@outlook.com>
2025-12-24 15:22:55 +0530

    Update main.tf

--- static_website/main.tf
+++ static_website/main.tf
@@ -46,3 +46,25 @@ resource "aws_s3_bucket_policy" "website" {
     ]
   })
 }
+
+resource "aws_s3_object" "website_files" {
+  for_each = fileset("${path.module}/www", "**/*")
+
+  bucket = aws_s3_bucket.website.id
+  key    = each.value
+  source = "${path.module}/www/${each.value}"
+  etag   = filemd5("${path.module}/www/${each.value}")
+  content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/javascript",
+    "json" = "application/json",
+    "png"  = "image/png",
+    "jpg"  = "image/jpeg",
+    "jpeg" = "image/jpeg",
+    "gif"  = "image/gif",
+    "svg"  = "image/svg+xml",
+    "ico"  = "image/x-icon",
+    "txt"  = "text/plain"
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
+}
