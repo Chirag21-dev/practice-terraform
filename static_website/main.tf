0c8bc4d0f5f61ed7ea3da98e733b61c9a8d807f3
Chirag21-dev <chiragks21@outlook.com>
2025-12-24 16:47:19 +0530

    Update main.tf

--- static_website/main.tf
+++ static_website/main.tf
@@ -110,3 +110,20 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     acm_certificate_arn = var.acm_us_east-1
   }
 }
+
+data "aws_route53_zone" "my_domain" {
+  name = local.my_domain
+}
+
+resource "aws_route53_record" "cloudfront" {
+  for_each = aws_cloudfront_distribution.s3_distribution.aliases
+  zone_id  = data.aws_route53_zone.my_domain.zone_id
+  name     = each.value
+  type     = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
